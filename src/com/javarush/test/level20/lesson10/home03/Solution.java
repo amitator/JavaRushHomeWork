a4d6537ab5891d62a08fb35c0697039ed9a9a91e
Igor Prus <prus.igor@gmail.com>
2016-05-29 23:56:25 +0300

    level20.lesson10.home03

---
+++ src/com/javarush/test/level20/lesson10/home03/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level20.lesson10.home03;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/* Найти ошибки
+Почему-то при сериализации/десериализации объекта класса B возникают ошибки.
+Найдите проблему и исправьте ее.
+Класс A не должен реализовывать интерфейсы Serializable и Externalizable.
+Сигнатура класса В не содержит ошибку :)
+Метод main не участвует в тестировании.
+*/
+public class Solution implements Serializable{
+    private static final long serialVersionUID = 1L;
+    public static class A {
+        private static final long serialVersionUID = 135L;
+        protected String name = "A";
+
+        public A(){}
+
+        public A(String name) {
+            this.name += name;
+        }
+    }
+
+    public class B extends A implements Serializable {
+        private static final long serialVersionUID = 3246531L;
+        public B(String name) {
+            super(name);
+            this.name += name;
+        }
+
+        private void writeObject(ObjectOutputStream out) throws IOException
+        {
+            out.defaultWriteObject();
+            out.writeObject(name);
+        }
+
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            in.defaultReadObject();
+            String n = (String) in.readObject();
+            name = n;
+        }
+    }
+}
